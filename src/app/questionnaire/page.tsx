6a9d997b7db870c5f21c091f734c5c9db98bc3e2
Nishant-B10 <162639290+Nishant-B10@users.noreply.github.com>
2025-09-27 03:51:02 -0400

    Update page.tsx
    removed .tsx

--- src/app/questionnaire/page.tsx
+++ src/app/questionnaire/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import EnhancedQuestionnaire from '@/components/EnhancedQuestionnaire.tsx'
+import EnhancedQuestionnaire from '@/components/EnhancedQuestionnaire'
 
 export default function Questionnaire() {
   return <EnhancedQuestionnaire />
